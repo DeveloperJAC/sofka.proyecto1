954336d76cd6a0ebc34750c754368bd66ad2266a
DeveloperJAC <connectionwweb@gmail.com>
2021-09-01 07:44:19 -0500

    Se creo el metodo main y el mostrar datos

--- src/main/java/com/sofka/proyecto1/Informacion.java
+++ src/main/java/com/sofka/proyecto1/Informacion.java
@@ -4,22 +4,27 @@ import java.util.Scanner;
 
 public class Informacion {
 
-    String nombre,celular;
+    String nombre, celular;
     int edad;
 
     public void leerInfo(){
         Scanner datos = new Scanner(System.in);
         System.out.println("Gracias por ingresar a nuestro sistema, para continuar le solicitaremos unos datos personales.");
         System.out.println("Escriba su nombre");
-        String nombre = datos.nextLine();
+        nombre = datos.nextLine();
         System.out.println("Escriba su numero de celular");
-        String celular = datos.nextLine();
+        celular = datos.nextLine();
         System.out.println("Escriba su edad");
-        int edad = datos.nextInt();
-
+        edad = datos.nextInt();
 
     }
+    public void mostrarDatos(){
 
+        System.out.println("Bienvenido señor "+nombre+ "," + " es un placer para nosotros contar con una persona de " + edad
+        + " años.");
+        System.out.println("Próximamente nos comunicaremos con usted al numero " + celular + "." + "\n" + "Feliz día" );
+
+    }
 
 
     }
---
+++ src/main/java/com/sofka/proyecto1/imp.java
@@ -0,0 +1,11 @@
+package com.sofka.proyecto1;
+
+public class imp {
+    public static void main(String[] args) {
+
+        Informacion inst = new Informacion();
+        inst.leerInfo();
+        inst.mostrarDatos();
+
+    }
+}
